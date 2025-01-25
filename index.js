d2dea2fc81d6bdbb1b9ed1f6500a6142eab23a92
John Kihugo Mwangi <johnmwangi13119@gmail.com>
2025-01-25 12:05:48 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+const main = document.getElementById("main");
+if (main) {
+  main.remove();
+}
+
+const newHeader = document.createElement("h1");
+
+
+newHeader.id = "victory";
+
+
+newHeader.innerHTML = "Your-Name is the champion"; 
+
+document.body.appendChild(newHeader);
